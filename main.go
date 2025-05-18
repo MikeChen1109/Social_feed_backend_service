c30d2e605b529b4df5debd14264eccb6e26621f4
Mike <mike.chen@joocatech.com>
2025-05-19 02:31:28 +0800

    refactor: restructure main function to initialize repositories, services, and middleware

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"myApp/SocialFeed/controllers"
 	"myApp/SocialFeed/initializers"
+	"myApp/SocialFeed/middleware"
+	"myApp/SocialFeed/repositories"
 	"myApp/SocialFeed/routes"
+	"myApp/SocialFeed/services"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,17 +14,26 @@ import (
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDatabase()
-	controllers.InitAuthController()
 }
 
 func main() {
-	sqlDB, _ := initializers.DB.DB()
+	// database connection
+	db := initializers.ConnectToDatabase()
+	sqlDB, _ := db.DB()
 	defer sqlDB.Close()
-	
+
+	// Initialize repositories, services, controllers, and middleware
+	userRepo := &repositories.UserRepository{DB: db}
+	feedsRepo := &repositories.FeedRepository{DB: db}
+	authService := &services.AuthService{UserRepo: userRepo}
+	feedService := &services.FeedService{FeedRepo: feedsRepo}
+	userController := &controllers.UsersController{AuthService: authService}
+	feedsController := &controllers.FeedsController{FeedsService: feedService}
+	authMiddleware := &middleware.AuthMiddleware{AuthService: authService}
+
+	// Initialize Gin router and register routes
 	router := gin.Default()
-
-	routes.RegisterFeedRoutes(router)
-	routes.RegisterUserRoutes(router)
-
+	routes.RegisterFeedRoutes(router, authMiddleware, feedsController)
+	routes.RegisterUserRoutes(router, userController)
 	router.Run()
 }
