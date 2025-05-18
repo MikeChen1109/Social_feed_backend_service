0112d5db1767912d88cd089be62792c4984ade39
Mike <mike.chen@joocatech.com>
2025-05-19 02:32:19 +0800

    refactor: simplify RequireAuth function by removing redundant token parsing logic

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,63 +1,36 @@
+// middleware/auth.go
 package middleware
 
 import (
-	"myApp/SocialFeed/initializers"
-	"myApp/SocialFeed/models"
+	"myApp/SocialFeed/services"
 	"net/http"
-	"os"
-	"time"
+	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 )
 
-func RequireAuth(c *gin.Context) {
+type AuthMiddleware struct {
+	AuthService *services.AuthService
+}
+
+// 回傳 gin.HandlerFunc
+func (m *AuthMiddleware) RequireAuth(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
-	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
-		c.Abort()
-		return
-	}
-
-	tokenString := ""
-
-	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
-		tokenString = authHeader[7:]
-	} else {
+	if !strings.HasPrefix(authHeader, "Bearer ") {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
 		c.Abort()
 		return
 	}
 
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("SECRET")), nil
-	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
 
-	if err != nil || !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+	user, err := m.AuthService.VerifyToken(tokenStr)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		c.Abort()
 		return
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		if float64(time.Now().Unix()) > claims["exp"].(float64) {
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-
-		if claims["sub"] == nil {
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-
-		var user models.User
-		initializers.DB.First(&user, claims["sub"])
-
-		if user.ID == 0 {
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-		
-		c.Set("user", user)
-		c.Next()
-	} else {
-		c.AbortWithStatus(http.StatusUnauthorized)
-	}
+	c.Set("user", *user)
+	c.Next()
 }
