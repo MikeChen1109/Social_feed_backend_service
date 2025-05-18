cdfa43ffa06ff05bd07fe6d28d66fbb693ef6482
Mike <mike.chen@joocatech.com>
2025-05-19 02:32:38 +0800

    refactor: update ConnectToDatabase function to return database instance directly

--- initializers/database.go
+++ initializers/database.go
@@ -8,14 +8,14 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
-func ConnectToDatabase() {
+func ConnectToDatabase() *gorm.DB {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("failed to connect to database")
 	}
+
+	return DB
 }
