fa01a94e047a3b9e344f443d3851f3a638447a3a
Mike <mike.chen@joocatech.com>
2025-05-27 01:20:44 +0800

    feat: Update feed and user service proxy URLs for development environment

--- api-gateway/handlers/feedHandler.go
+++ api-gateway/handlers/feedHandler.go
@@ -9,7 +9,8 @@ import (
 
 func ProxyToFeedService(c *gin.Context) {
 	if os.Getenv("APP_ENV") == "dev" {
-		utils.ProxyRequest(c, "http://localhost:3000")
+		// utils.ProxyRequest(c, "http://localhost:3000")
+		utils.ProxyRequest(c, "http://feed-service:3000")
 	} else {
 		utils.ProxyRequest(c, os.Getenv("FEED_SERVICE_URL"))
 	}
--- api-gateway/handlers/userHandler.go
+++ api-gateway/handlers/userHandler.go
@@ -9,7 +9,8 @@ import (
 
 func ProxyToUserService(c *gin.Context) {
 	if os.Getenv("APP_ENV") == "dev" {
-		utils.ProxyRequest(c, "http://localhost:4000")
+		// utils.ProxyRequest(c, "http://localhost:4000")
+		utils.ProxyRequest(c, "http://user-service:4000")
 	} else {
 		utils.ProxyRequest(c, os.Getenv("FEED_SERVICE_URL"))
 	}
